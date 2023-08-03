d9af15d65909381abbe4de1aa60977c22404a8ba
Md-TarikulIslam <nayon1820@gmail.com>
2023-08-03 15:51:59 +0600

    3rd Aug, 2023

--- src/components/Pages/Homepage/Homepage.jsx
+++ src/components/Pages/Homepage/Homepage.jsx
@@ -1,68 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import nav from '../../../images/nav.png'
 import foo from '../../../images/foo.png'
+import axios from "axios";
 
 const Homepage = () => {
-  const fakeFootballData = [
-    {
-      id: 1,
-      team1: 'Barcelona',
-      goals: 50,
-      shots: 180,
-      possession: 65,
-    },
-    {
-      id: 2,
-      team2: 'Real Madrid',
-      goals: 95,
-      shots: 150,
-      possession: 55,
-    },
+  const [stats, setStats] = useState([]);
+  const [selectedDate, setSelectedDate] = useState("");
+  const fetchStatsByDate = async () => {
+    try {
+      const response = await axios.get(`https://save-football-api.vercel.app/api/fixtures/${selectedDate}`);
 
-  ];
+      setStats(response.data);
+      console.log(response.data)
+
+      console.log(response.data[0].response[0].fixture.id)
+    } catch (error) {
+      console.error("Error fetching stats:", error);
+    }
+  };
+
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchStatsByDate();
+  };
+
+
   return (
     <div>
       <div>
         <img className='w-full' src={nav} alt="" />
-        <h1 className='card-title text-4xl max-w-7xl mx-auto my-5 text-blue-600'>Stats</h1>
 
-        <div className='card card-body mb-10 bg-base-100 max-w-4xl mx-auto shadow-xl'>
-          <div className='flex   flex-col lg:flex-row justify-between max-w-3xl mx-auto gap-5'>
-            {fakeFootballData.map((teamStats, index) => (
-              <div key={index} className=''>
-                <p className='font-bold text-xl  text-left'>{teamStats.team1}</p>
-                <p className='font-bold text-xl  text-right'>{teamStats.team2}</p>
-                <p className='text-center mt-5'>Shots on target</p>
-                <p>{teamStats.goals}<progress className="progress w-96" id={teamStats.id} value={teamStats.goals} max="100"></progress></p>
-
-                <p className='text-center mt-5'>Shot off target</p>
-                <p>{teamStats.shots}<progress className="progress w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Blocked Shots</p>
-                <p>{teamStats.shots}<progress className="progress w-96" id={teamStats.id} value={teamStats.shots} max="300"></progress></p>
-                <p className='text-center mt-5'>Possession (%)</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Corner Kicks</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Offsides</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Fouls</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Throw ins</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Yellow Cards</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Red cards</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Goalkeeper saves</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-                <p className='text-center mt-5'>Goal kicks</p>
-                <p>{teamStats.shots}<progress className="progress  w-96" id={teamStats.id} value={teamStats.shots} max="200"></progress></p>
-
-
-              </div>
-            ))}
+        <form onSubmit={handleSubmit} className='text-center mx-auto max-w-4xl'>
+          <h1 className='font-bold text-4xl text-center my-5 text-blue-600'>Stats</h1>
+          <div className='flex items-center  justify-between mx-auto'>
+            <p>Select date</p>
+            <input
+              type="date"
+              value={selectedDate}
+              onChange={handleDateChange}
+              className='input input-bordered  w-full max-w-lg'
+            />
+            <button type="submit" className='btn btn-neutral'>Fetch Stats</button>
           </div>
-        </div>
+        </form>
+        {
+          stats && <div className='max-w-4xl mx-auto my-5'>
+            {stats.length > 0 ? (
+              <div className='card card-body mb-10 bg-base-100 shadow-2xl'>
+                {stats.map((stat) => (
+                  <div key={stat.response[0].fixture.id}>
+                    <div className='flex justify-between'>
+                      <div className='flex items-center gap-5'>
+                        <p className='font-bold text-xl  '>{stat.response[0].teams.home.name}</p>
+                        <img className='w-10' src={stat.response[0].teams.home.logo} alt="" />
+                      </div>
+                      <div className='flex items-center gap-5'>
+                        <img className='w-10' src={stat.response[0].teams.away.logo} alt="" />
+                        <p className='font-bold text-xl '>{stat.response[0].teams.away.name}</p>
+
+                      </div>
+                    </div>
+                    <p className='text-center mt-5'>Half Time Goal</p>
+                    <div className='flex justify-between'>
+                      <div className='flex flex-col gap-2'>
+                        <p>{stat.response[0].score.halftime.home}</p><progress className="progress w-96" value={stat.response[0].score.halftime.home} max="7"></progress>
+                      </div>
+                      <div className='flex flex-col gap-2'>
+                        <p>{stat.response[0].score.halftime.away}</p><progress className="progress w-96" value={stat.response[0].score.halftime.away} max="7"></progress>
+                      </div>
+                    </div>
+                    <p className='text-center mt-5'>Total Goals</p>
+                    <div className='flex justify-between'>
+                      <div className='flex flex-col gap-2'>
+                        <p>{stat.response[0].goals.home}</p><progress className="progress w-96" value={stat.response[0].goals.home} max="7"></progress>
+                      </div>
+                      <div className='flex flex-col gap-2'>
+                        <p>{stat.response[0].goals.away}</p><progress className="progress w-96" value={stat.response[0].goals.away} max="7"></progress>
+                      </div>
+                    </div>
+
+
+                  </div>
+                )
+                )
+                }
+              </div>
+
+            ) : (
+              <p>No stats to display.</p>
+            )}
+
+          </div>
+        }
+
+
         <img className='w-full' src={foo} alt="" />
       </div>
     </div>
